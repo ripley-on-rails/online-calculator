931390a25b74826968d5850f0eba36decd09eb36
Ripley Flammer <flammer.ripley@gmail.com>
2021-05-06 00:37:36 +0200

    Make uberjar buildable

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject online-calculator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An online calculator"
+  :url "https://github.com/phalphalak/online-calculator"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
@@ -11,4 +11,6 @@
                  [ring/ring-jetty-adapter "1.9.2"]
                  [ring/ring-defaults "0.3.2"]
                  [compojure "1.6.2"]]
+  :main online-calculator.core
+  :profiles {:uberjar {:aot :all}}
   :repl-options {:init-ns online-calculator.core})
